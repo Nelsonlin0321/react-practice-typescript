3e5888432e4f656294cd0422fd9d5e95d23ffaf9
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-09-16 10:27:56 +0800

    fetching data using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-import ProductList from "./components/ProductList";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [category, setCategory] = useState<string>("");
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
 
   return (
     <>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
